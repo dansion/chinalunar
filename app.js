73c34d9e15eb3acf4dc708de1194af3e9a32eb1b
合沙 <hesha@alibaba-inc.com>
2023-11-29 16:30:09 +0800

    add youmeng

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import '/hesha'
+
+import 'umtrack-alipay';
 App({
   onLaunch(options) {
     // 第一次打开
@@ -9,4 +10,10 @@ App({
     // 从后台被 scheme 重新打开
     // options.query == {number:1}
   },
+  //
+  umengConfig: {
+    appKey: '6566effeb2f6fa00ba88e30b', //由友盟分配的APP_KEY
+    debug: true, //是否打开调试模式
+    uploadUserInfo: true // 自动上传用户信息，设为false取消上传，默认为false
+  }
 });
